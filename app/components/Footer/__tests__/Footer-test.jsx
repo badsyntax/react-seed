297aba224351c5e527eab7a211187145183f86b8
Richard Willis <willis.rh@gmail.com>
2015-07-22 20:35:12 +0100

    Fixed tests

--- app/components/Footer/__tests__/Footer-test.jsx
+++ app/components/Footer/__tests__/Footer-test.jsx
@@ -5,11 +5,11 @@ import { expect } from 'chai';
 let { TestUtils } = React.addons;
 
 describe('Footer', () => {
-  it('Should have the correct css class', () => {
+  it('Should have the correct footer element', () => {
     let footer = TestUtils.renderIntoDocument(
       <Footer />
     );
     let footerElem = React.findDOMNode(footer);
-    expect(footerElem.className).to.equal('footer');
+    expect(footerElem.tagName.toLowerCase()).to.equal('footer');
   });
 });
